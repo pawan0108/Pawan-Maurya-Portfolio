6558a9525e9233b613e2cbcd64362680716424c4
Pawan Maurya <pawanmaurya8931@gmail.com>
2025-08-06 01:55:35 +0800

    Updated project with new changes

--- experience/script.js
+++ experience/script.js
@@ -31,15 +31,7 @@ srtop.reveal('.experience .timeline .container',{interval: 400});
 
 
 // Start of Tawk.to Live Chat
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+
 // End of Tawk.to Live Chat
 
 
@@ -62,14 +54,3 @@ document.onkeydown = function(e) {
   }
 }
 
-document.addEventListener('visibilitychange',
-function(){
-    if(document.visibilityState === "visible"){
-        document.title = "Experience | Portfolio Jigar Sable";
-        $("#favicon").attr("href","/assets/images/favicon.png");
-    }
-    else {
-        document.title = "Come Back To Portfolio";
-        $("#favicon").attr("href","/assets/images/favhand.png");
-    }
-});
